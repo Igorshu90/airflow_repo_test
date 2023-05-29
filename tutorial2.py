428d8e2acccb9416dcb8f07d3220425d1f245b90
Igor <igorshu90@gmail.com>
2023-05-29 18:09:03 +0300

    some message

--- tutorial2.py
+++ tutorial2.py
@@ -12,10 +12,13 @@ from utils.utils_main import test
 local_tz = pendulum.timezone('Israel')
 
 default_args = {
-    "owner": "Igor",
+    "owner": "Igor3",
     "start_date": datetime(2015, 6, 1, tzinfo=local_tz)
 }
 
 with DAG(dag_id='igor_test_dag', catchup=False, default_args=default_args, schedule_interval='27 13 * * *') as dag:
     op1 = BashOperator(task_id='op1', bash_command='echo bla bla')
     op2 = PythonOperator(task_id='op2', python_callable=test)
+    op3 = PythonOperator(task_id='op2', python_callable=test)
+
+    op1 >> op2 >> op3
